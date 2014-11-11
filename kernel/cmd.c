0098b075000fa40ac5542d80770036afa2aa1952
zuban32 <zuban32s@gmail.com>
2014-11-11 10:24:39 +0400

    Added 'help' command

--- kernel/cmd.c
+++ kernel/cmd.c
@@ -12,6 +12,9 @@ extern struct idt_entry idt_tbl[];
 char cmd_name[CMD_NAME_LEN + 1];
 uint32_t params[2];
 
+const char *cmds[CMD_NUM] = {"isr", "listisr", "int", "clear", "help"};
+const char *cmd_help[CMD_NUM] = {"create ISR #param0", "list all existing ISRs", "equal to asm int #param0", "clear screen", "show this help"};
+
 #define TESTBASE 0x1000000
 
 char *test = TESTBASE;
@@ -26,12 +29,12 @@ cmd(void)
 
 	kprintf(1, "Cmd got: %s\n", kbd_buf);
 	parse_cmd();
-	if(kstrcmp(cmd_name, "isr") == 0)
+	if(kstrcmp(cmd_name, "isr") == 0)				//REWRITE ALL THIS SHIT
 	{
 		kprintf(1, "isr #%d will be created\n", params[0]);
 		addISR(params[0], 0x8, i386_GATE);
 	}
-	else if(kstrcmp(cmd_name, "listint") == 0)
+	else if(kstrcmp(cmd_name, "listisr") == 0)
 	{
 		for(int i = 0; i < IDTSIZE; i++)
 			if(idt_tbl[i].offset1 || idt_tbl[i].offset2)
@@ -46,6 +49,11 @@ cmd(void)
 	}
 	else if(kstrcmp(kbd_buf, "clear") == 0)
 		kclear_screen();
+	else if(kstrcmp(kbd_buf, "help") == 0)
+	{
+		for(int i = 0; i < CMD_NUM; i++)
+			kprintf(1, "%s - %s\n", cmds[i], cmd_help[i]);
+	}
 	else
 		kprintf(1, "Unknown command\n");
 }
--- kernel/cmd.h
+++ kernel/cmd.h
@@ -4,6 +4,8 @@
 #define CMD_NAME_LEN 80
 #define MAX_PARAMS 2
 
+#define CMD_NUM 5
+
 void cmd(void);
 void parse_cmd(void);
 
